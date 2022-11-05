e795904d7a464268f26abb96c7cc396f263ea855
Guilherme de Paula <guilherme1.depaula@gmail.com>
2022-11-05 09:49:31 -0300

    integrating Firebase Hosting

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ import { useState } from 'react';
 import { generateColors } from './helpers/generateColors';
 import { ModalTutorial } from './components/ModalTutorial';
 import './App.css';
+import { initializeApp } from "firebase/app";
 
 function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyApEZnxxBoRtNm-EB6OKU5SumOXD8me1yg",
+    authDomain: "color-generator-70a74.firebaseapp.com",
+    projectId: "color-generator-70a74",
+    storageBucket: "color-generator-70a74.appspot.com",
+    messagingSenderId: "799425174298",
+    appId: "1:799425174298:web:70aeab5b5cd1e226e82aa3"
+  };
+  const app = initializeApp(firebaseConfig);
   const [currentColor, setCurrentColor] = useState(generateColors.simpleColor());
   const [currentType, setCurrentType] = useState("simple")
   const [copyMessage, setCopyMessage] = useState(false)
