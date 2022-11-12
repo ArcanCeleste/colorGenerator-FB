b5f4c5d42dc30fca33eb8e67303692916738c379
Guilherme de Paula <guilherme1.depaula@gmail.com>
2022-11-12 12:53:50 -0300

    Adding firebase folder

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyApEZnxxBoRtNm-EB6OKU5SumOXD8me1yg",
+    authDomain: "color-generator-70a74.firebaseapp.com",
+    projectId: "color-generator-70a74",
+    storageBucket: "color-generator-70a74.appspot.com",
+    messagingSenderId: "799425174298",
+    appId: "1:799425174298:web:70aeab5b5cd1e226e82aa3"
+};
+export const fbConfig = initializeApp(firebaseConfig);
